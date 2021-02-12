13290574f9b393a8643da8c8342af586a3963889
David <davemgj@gmail.com>
2021-02-11 21:07:18 -0800

    simple show/setshow state add to haiku container

--- src/components/Haiku.jsx
+++ src/components/Haiku.jsx
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Haiku.scss";
 
 const Haiku = () => {
-  return (
-    <section className="haiku-container">
-      <div>
-        <p>A huge frog and I</p>
-        <p>staring at each other</p>
-        <p>neither of us moves</p>
-      </div>
-      <button>HAIKU</button>
-    </section>
-  );
+  const [show, setShow] = useState(false);
+
+  if (show) {
+    return (
+      <section className="haiku-container">
+        <div>
+          <p>A huge frog and I</p>
+          <p>staring at each other</p>
+          <p>neither of us moves</p>
+        </div>
+        <button>HAIKU ME!</button>
+      </section>
+    );
+  } else {
+    return (
+      <section className="haiku-container">
+        <button onClick={() => setShow(true)}>HAIKU ME!</button>
+      </section>
+    );
+  }
 };
 
 export default Haiku;
